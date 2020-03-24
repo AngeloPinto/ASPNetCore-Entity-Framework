5453f62b148b38c60fc010b526249d85f5b7e8d2
Angelo <angelorpt@gmail.com>
2020-03-24 17:10:36 -0400

    Populando a tabela Categorias

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,16 +6,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CSharpEF.Models;
+using CSharpEF.Database;
 
 namespace CSharpEF.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly ApplicationDBContext database;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDBContext database)
         {
-            _logger = logger;
+            this.database = database;
         }
 
         public IActionResult Index()
@@ -26,6 +30,31 @@ namespace CSharpEF.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Teste()
+        {
+            Categoria c1 = new Categoria();
+            Categoria c2 = new Categoria();
+            Categoria c3 = new Categoria();
+            Categoria c4 = new Categoria();
+            
+            c1.Nome = "Aluno";
+            c2.Nome = "Professor";
+            c3.Nome = "Monitor";
+            c4.Nome = "Monitor";
+
+            List<Categoria> lstCategoria = new List<Categoria>();
+
+            lstCategoria.Add(c1);
+            lstCategoria.Add(c2);
+            lstCategoria.Add(c3);
+            lstCategoria.Add(c4);
+
+            database.AddRange(lstCategoria);
+            database.SaveChanges();
+
+            return Content("Dados salvos");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
