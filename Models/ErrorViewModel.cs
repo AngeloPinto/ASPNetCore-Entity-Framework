5e2d99f48d60ddb74bd1d23dc017d49a777c7bba
Angelo <angelorpt@gmail.com>
2020-03-24 09:15:32 -0400

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSharpEF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
