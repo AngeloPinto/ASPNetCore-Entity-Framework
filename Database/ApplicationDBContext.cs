fe4eadbba5f73342a1cc6be0cacc6f4b70fda003
Angelo <angelorpt@gmail.com>
2020-03-24 17:47:22 -0400

    Adicionando o Model de Produto e referenciando a Categoria

--- Database/ApplicationDBContext.cs
+++ Database/ApplicationDBContext.cs
@@ -8,6 +8,7 @@ namespace CSharpEF.Database
 
         public DbSet<Funcionario> Funcionarios { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) 
         : base(options) 
         {
--- Migrations/20200324214607_AddProduto.Designer.cs
+++ Migrations/20200324214607_AddProduto.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CSharpEF.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CSharpEF.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20200324214607_AddProduto")]
+    partial class AddProduto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
