7b9b2f1aa2cdff478057c8af0cb0aefabd2164e5
Angelo <angelorpt@gmail.com>
2020-03-24 14:31:36 -0400

    Salvando dados do formulário no banco de dados

--- Database/FuncionarioController.cs
+++ Database/FuncionarioController.cs
@@ -1,11 +1,18 @@
 using System;
 using CSharpEF.Models;
 using Microsoft.AspNetCore.Mvc;
+using CSharpEF.Database;
 
 namespace CSharpEF.Database
 {
     public class FuncionarioController : Controller
     {
+        private readonly ApplicationDBContext database;
+
+        public FuncionarioController(ApplicationDBContext database)
+        {
+            this.database = database;
+        }
         public IActionResult Cadastrar() 
         {
             return View();
@@ -14,7 +21,9 @@ namespace CSharpEF.Database
         [HttpPost]
         public IActionResult Salvar(Funcionario funcionario)
         {
-            return Content(funcionario.Nome.ToString() + " " + funcionario.Salario + " " + funcionario.Cpf);
+            database.Funcionarios.Add(funcionario);
+            database.SaveChanges();
+            return Content("Funcionário Salvo com Sucesso!!!");
         }
     }
 
