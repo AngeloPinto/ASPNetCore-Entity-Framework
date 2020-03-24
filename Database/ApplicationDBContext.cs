40dde33b0a296c0a680c6371fc24b6c77d9a931c
Angelo <angelorpt@gmail.com>
2020-03-24 09:35:48 -0400

    Configurando o Entity Framework com Mysql no Projeto

---
+++ Database/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharpEF.Database
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) 
+        : base(options) 
+        {
+            
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using CSharpEF.Database;
 
 namespace CSharpEF
 {
@@ -23,6 +25,7 @@ namespace CSharpEF
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDBContext>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
