22efcf57f07261c09da939818c58639e15999e9a
Angelo <angelorpt@gmail.com>
2020-03-25 09:36:56 -0400

    Utilizando Fluent API

--- Database/ApplicationDBContext.cs
+++ Database/ApplicationDBContext.cs
@@ -11,9 +11,19 @@ namespace CSharpEF.Database
         public DbSet<Produto> Produtos { get; set; }
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
 
+        // Utilizando método Lazy Moding
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        // Customização das tabelas
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>().ToTable("ProdutosV2");
+            // modelBuilder.Entity<Produto>().Haskey(p. => p.CodigoBarras); // Definindo outra chave primária
+            modelBuilder.Entity<Produto>().Property(p => p.Nome).IsRequired();
+            modelBuilder.Entity<Produto>().Property(p => p.Nome).HasMaxLenght(100);
         }
     }
 }
